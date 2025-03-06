58a63e9197ba98bdd16430bc6d9e4ad261f028b0
Nadezhda Iarygina <nadezhda.yarygina@gmail.com>
2025-03-06 02:39:57 -0500

    update emotion_detector function to return json

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,10 +1,17 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyze: str) -> str:
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = {"raw_document": {"text": text_to_analyze}}
 
     response = requests.post(url, json = input_json, headers = header, timeout = 5)
-    
-    return response.text
+    response_json = json.loads(response.text)
+
+    analysis = {}
+    for emotion, score in response_json["emotionPredictions"][0]["emotion"].items():
+        analysis[emotion] = score
+    analysis["dominant_emotion"] = max(analysis, key = analysis.get)
+
+    return analysis
